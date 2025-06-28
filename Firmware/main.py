6adbd67079659678aa2152a602862050b2eebfe3
Alfonso <alfonsocespino10@gmail.com>
2025-06-28 12:55:59 -0700

    Update main.py

--- Firmware/main.py
+++ Firmware/main.py
@@ -10,7 +10,7 @@ keyboard = KMKKeyboard()
 macros = Macros()
 keyboard.modules.append(macros)
 
-keyboard.col_pins = (board.GP0, board.GP1, board.GP2, board.GP3, board.GP4, board.GP5, board.GP6, board.GP7, board.GP8, board.GP9, board.GP10, board.GP11, board.GP12, board.GP13, board.GP14, board.GP15, board.GP16, board.GP17)
+keyboard.col_pins = (board.GP0, board.GP1, board.GP2, board.GP3, board.GP4, board.GP5, board.GP6, board.GP7, board.GP8, board.GP9, board.GP10, board.GP11, board.GP12, board.GP13, board.GP14, board.GP15, board.GP16, board.GP17, board.GP18)
 keyboard.row_pins = (board.GP18, board.GP19, board.GP20, board.GP21, board.GP22)
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
